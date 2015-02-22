acce1a169f9fe37444558b4113fcb7a4313dcb5e
Joseph McGee <jmmcgee.00@gmail.com>
2015-02-22 02:53:36 -0800

    pareto.py automatically generates graphs for utilization and mean_queue_length

--- pareto.py
+++ pareto.py
@@ -25,7 +25,45 @@ def __main__(MAX_BUFFER, PARAM_MU, PARAM_LAMBDA):
 
     simulation = Simulation(EVENTS_TO_SIMULATE, MAX_BUFFER, ARRIVAL_INTERVAL, PROCESSING_INTERVAL, quiet=True)
     simulation.run()
-    (dropped, utilization, mean_queue_length) = simulation.get_statistics()
+    return simulation.get_statistics()
 
-if __name__ == '__main__':
-    __main__(*sys.argv[1:])
+    
+#if __name__ == '__main__':
+#    __main__(*sys.argv[1:])
+
+class Plot:
+    x_vals = []
+    y_vals = []
+
+departure_rate = 1
+plt.figure(1)
+
+utilized_plot = Plot()
+utilized_plot.x_vals = []
+utilized_plot.y_vals = []
+for arrival_rate in (0.1,0.25,0.4,0.55,0.65,0.8,0.9):
+    (dropped, utilization, mean_queue_length) = __main__(0, arrival_rate, departure_rate)
+    utilized_plot.x_vals.append(arrival_rate)
+    utilized_plot.y_vals.append(utilization)
+print utilized_plot.x_vals
+print utilized_plot.y_vals
+plt.subplot(211)
+plt.plot(utilized_plot.x_vals, utilized_plot.y_vals, 'ro')
+plt.title('Utilization of M/M/1 Queue with Infinite buffer')
+plt.ylabel('utilization')
+
+mean_queue_length_plot = Plot()
+mean_queue_length_plot.x_vals = []
+mean_queue_length_plot.y_vals = []
+for arrival_rate in (0.1,0.25,0.4,0.55,0.65,0.8,0.9):
+    (dropped, utilization, mean_queue_length) = __main__(0, arrival_rate, departure_rate)
+    mean_queue_length_plot.x_vals.append(arrival_rate)
+    mean_queue_length_plot.y_vals.append(mean_queue_length)
+print mean_queue_length_plot.x_vals
+print mean_queue_length_plot.y_vals
+plt.subplot(212)
+plt.plot(mean_queue_length_plot.x_vals, mean_queue_length_plot.y_vals, 'ro')
+plt.title('Mean Queue length of M/M/1 Queue with Infinite buffer')
+plt.xlabel('arrival_rate')
+plt.ylabel('mean_queue_length')
+plt.show()
