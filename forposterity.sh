40c17fab07629494ea02611894804998501f5d4e
Joseph McGee <jmmcgee.00@gmail.com>
2015-02-24 13:57:10 -0800

    Added part1-2 to script and results

--- forposterity.sh
+++ forposterity.sh
@@ -1,3 +1,15 @@
+MAXBUFFER=0
+echo "====================="
+echo MAXBUFFER=${MAXBUFFER}
+echo
+for x in 0.1 0.25 0.4 0.55 0.65 0.8 0.9; do
+    echo ", ARRIVAL_RATE=$x";
+    python phase1.py $MAXBUFFER 1 $x;
+    echo 
+done;
+echo "====================="
+echo
+
 for MAXBUFFER in 1 20 50; do
     echo "====================="
     echo MAXBUFFER=${MAXBUFFER}
