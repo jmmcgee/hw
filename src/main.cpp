9f1bcce7263993e7d376ccbd94b991e64ed77845
Joseph McGee <jmmcgee.00@gmail.com>
2015-04-04 23:15:23 -0700

    Began working on getUserInput. Incomplete

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,13 @@
 using namespace std;
 
 void displayPrompt();
+void getUserInput();
 
 int main(int argc, char *argv[])
 {
   SetNonCanonicalMode(STDIN_FILENO, new termios);
   displayPrompt();
+  getUserInput();
 }
 
 void displayPrompt()
@@ -29,4 +31,26 @@ void displayPrompt()
   }
   write(STDOUT_FILENO, path.data(), path.size());
   write(STDOUT_FILENO, "> ", 2);
+}
+
+void getUserInput()
+{
+  char c;
+  size_t input_count = 0;
+
+  // TODO: implement delete (escape-[-3-~)
+
+  while((read(STDIN_FILENO, &c, 1) != -1) && (c != '\n')) {
+    switch(c) {
+      case '\b':
+        if(input_count) {
+          --input_count;
+          write(STDOUT_FILENO, "\b \b", 3);
+        }
+        break;
+      default:
+        ++input_count;
+        write(STDOUT_FILENO, &c, 1);
+    }
+  }
 }
