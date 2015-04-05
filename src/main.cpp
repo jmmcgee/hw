b99aafe761a57e4800ae31b8108b6ef3ffb30edd
Joseph McGee <jmmcgee.00@gmail.com>
2015-04-04 21:35:57 -0700

    Implemented displayPrompt.
    displayPrompt prints the directory to as well as the caret to the screen.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
+#include <string>
+
+using namespace std;
+
+void displayPrompt();
 
 int main(int argc, char *argv[])
 {
+  displayPrompt();
+
   system("echo Hello World!!");
+}
+
+void displayPrompt()
+{
+  // Display prompt
+  char* cpath = get_current_dir_name();
+  string path(cpath != nullptr ? cpath : "");
+  free(cpath);
+
+  if(path.size() > 16) {
+    size_t lastSep = path.rfind('/');
+    path = "/..." + path.substr(lastSep);
+  }
+  write(STDOUT_FILENO, path.data(), path.size());
+  write(STDOUT_FILENO, "> ", 2);
 }
