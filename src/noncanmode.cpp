56bfcf5e3ca5b6db8f033e0c93876eeafe423d18
Joseph McGee <jmmcgee.00@gmail.com>
2015-04-12 19:56:03 -0700

    Removed noncanonical_main, unused and contained disallowed printf

--- src/noncanmode.cpp
+++ src/noncanmode.cpp
@@ -32,28 +32,3 @@ void SetNonCanonicalMode(int fd, struct termios *savedattributes){
     tcsetattr(fd, TCSAFLUSH, &TermAttributes);
 }
 
-
-int canonical_main(int argc, char *argv[]){
-    struct termios SavedTermAttributes;
-    char RXChar;
-    
-    SetNonCanonicalMode(STDIN_FILENO, &SavedTermAttributes);
-    
-    while(1){
-        read(STDIN_FILENO, &RXChar, 1);
-        if(0x04 == RXChar){ // C-d
-            break;
-        }
-        else{
-            if(isprint(RXChar)){
-                printf("RX: '%c' 0x%02X\n",RXChar, RXChar);   
-            }
-            else{
-                printf("RX: ' ' 0x%02X\n",RXChar);
-            }
-        }
-    }
-    
-    ResetCanonicalMode(STDIN_FILENO, &SavedTermAttributes);
-    return 0;
-}
