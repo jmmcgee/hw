50d98c04b8dc9b363b839eba1cad0ec5c4cc60f2
Joseph McGee <jmmcgee.00@gmail.com>
2015-04-28 03:07:28 +0100

    added Part 3

--- main.cpp
+++ main.cpp
@@ -32,7 +32,10 @@
 #include <Adafruit_GFX.h>
 #include "Adafruit_SSD1351.h"
 #include <SPI_OLED.h>
-#include  <WyzBee_spi.h>
+#include <WyzBee_spi.h>
+#include <WyzBee_i2c.h>
+#include <WyzBee_gpio.h>
+#include <locale.h>
 
 
 
@@ -49,7 +52,7 @@ extern Adafruit_SSD1351 tft = Adafruit_SSD1351(); //@  OLED class variable
  */
 
 
-WyzBeeSpi_Config_t  config_stc={
+WyzBeeSpi_Config  config_stc={
 		4000000,
 		SpiMaster,
 		SpiSyncWaitZero,
@@ -61,22 +64,58 @@ WyzBeeSpi_Config_t  config_stc={
 		NULL
 };
 
+WyzBeeI2c_Config config_i2c={
+    100000,
+    I2cMasterMode,
+    I2cNoizeFilterLess100M,
+    0x00
+};
+    
+
 
 int main()
-{
-	WyzBeeSpi_Init(&config_stc); 
-    Adafruit_SSD1351 myOled = Adafruit_SSD1351();//Constructor
-    myOled.begin();  //@ OLED screen Initialization
-    
+{    
+	WyzBeeSpi_Init(&config_stc); // initialize spi 
+    WyzBeeI2c_Init(&config_i2c); // initialize i2c
+    Adafruit_SSD1351 myOled;
+    myOled.begin(); //@ OLED screen Initialization
+
+    // Prepare screen
     myOled.fillScreen(BLACK); //@ fills the OLED screen with black pixels
-    myOled.setCursor(15, 15); //@ Set the print cursor to the initial location
-    myOled.setTextSize(2); //@ Set the text size on the OLED
-    myOled.setTextColor(RED, BLACK);
-    myOled.writeString("Iris eats weirdos...");
-    myOled.setTextColor(CYAN, BLACK);
-    myOled.writeString("\nlike Joseph for breakfast."); //@ Write a character to the screen
-    
-    while(1);
+    myOled.setTextSize(1); //@ Set the text size on the OLED
+    myOled.setTextColor(WHITE, BLACK);
+
+    while(1) {
+        if(!WyzBeeGpio_Get(4E))
+            continue;
+        
+        uint8_t MEASURE_HUMID[1] = {0xE5};
+        uint8_t MEASURE_TEMP[1] = {0xE3};
+        
+        char buf[64]={0};
+        uint8_t addr = 0x40;
+        uint8_t data[4]={0};
+        uint16_t len=4;
+        int c=0;
+        
+        WyzBeeI2c_Write(addr, MEASURE_HUMID, 1);
+        WyzBeeI2c_Read(addr, data, &len);
+        int16_t humidCode = ((int16_t)data[0]<<8)|data[1];
+        double humid = (125*(double)humidCode)/65536 - 6;
+        c += sprintf(buf+c, "Humidity = %.2lf%%\n", humid);
+        buf[c-5] = '.'; // dirty dirty hack
+
+
+        WyzBeeI2c_Write(addr, MEASURE_TEMP, 1);
+        WyzBeeI2c_Read(addr, data, &len);
+        int16_t tempCode = ((int16_t)data[0]<<8)|data[1];
+        double temp = (175.72*(double)tempCode)/65536 - 46.85;
+        c += sprintf(buf+c, "Temperature = %.2lf\n", temp);
+        buf[c-4] = '.'; // dirty dirty hack
+        
+        myOled.setCursor(0, 0); //@ Set the print cursor to the initial location
+        myOled.writeString(buf);
+    }    
 }
 /*
  *********************************************************************************************************
