507132b4fca606e88cfdc0435c172e0e9d8f43d9
Joseph McGee <jmmcgee.00@gmail.com>
2015-05-08 04:08:42 +0100

    Successfully set bt name and display to OLED.

--- App/main.cpp
+++ App/main.cpp
@@ -8,18 +8,19 @@
 
 #include "lab2_int.h"
 
+
+char* buf = new char[336];
+int n = 0;
 int main()
 {
   initOled();
   sys_ticks_init();
   WyzBee_BT_init();
-
-  char* buf = new char[336];
-  int n = 0;
+	WyzBee_SetLocalName((uint8_t*)"jmmcgee-071");
+	
+	char* name = new char[32];
+	WyzBee_GetLocalName((uint8_t*)name);
+  n = strlen(name);
   
-  buf = "OLED workds";
-  n = strlen(buf);
-  
-  oled.writeString(buf, n);
-  WyzBeeGpio_Init(GPIO_LED2,GPIO_OUTPUT,GPIO_LOW);
+  oled.writeString(name, n);
 }
