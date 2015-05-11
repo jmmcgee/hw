077e983e509a0b5e752d64f6dd8d77e9568abb95
Joseph McGee <jmmcgee.00@gmail.com>
2015-05-11 08:31:09 -0700

    Cleaned up debug code. Moved print statemtns into flush, printRetVals, printDevices

--- App/main.cpp
+++ App/main.cpp
@@ -8,67 +8,90 @@
 
 #include "lab2_int.h"
 
+void printDevices();
+void printRetVals(char* buf, int& n);
+void flush();
 
 int n = 0;
+char buf[336] = {0};
+const char* name = "jmcgee-071";
+#define NUM_DEVICES 5
+RSI_BT_EVENT_INQUIRY_RESPONSE res[NUM_DEVICES];
+int ret[10] = {9};
+
 int main()
 {
-	char buf[336] = {0};	
-	RSI_BT_EVENT_INQUIRY_RESPONSE* res;
-	uint8 size = 5;
-	res = new RSI_BT_EVENT_INQUIRY_RESPONSE[5];
-	int ret[10] = {0};
-
   initOled();
   sys_ticks_init();
   WyzBee_BT_init();
-	WyzBee_SetLocalName((uint8_t*)"jmmcgee-071");
+	WyzBee_SetLocalName((uint8_t*)name);
 
 	// Get Name
 	WyzBee_GetLocalName((uint8_t*)buf);
   n = strlen(buf);
-
+	n += sprintf(buf+n, "\n");
+	flush();
+	
 	// Set Discoverable, Connectable, Init
 	ret[0] = WyzBee_SetDiscoverMode(1, 500);
 	ret[1] = WyzBee_SetConnMode(1);
 	ret[2] = WyzBee_InitSppProfile();
+	flush();
 	
 	setColor(R);
 
 	// Find Devices
-	ret[3] = WyzBee_GetInquiryResults(res, size);
+	ret[3] = WyzBee_GetInquiryResults(res, NUM_DEVICES);
+	printDevices();
+	flush();
 
 	setColor(R_OFF);
 	setColor(G);
-	
-	// Print Available Devices
-	n += sprintf(buf+n, "\n");
-	for(int i = 0; i < size; i++) {
-		n += sprintf(buf+n, "%d) %s\n", i, res->RemoteDeviceName);
-	}
 
-	setColor(G_OFF);
-	setColor(B);
 	
 	// Choose device and print Address
 	int deviceNum = 0;
 	uint8_t bt_str[18] = {0};
 	BT_BDAddrToStr((res+deviceNum)->BDAddress, bt_str);
 	n += sprintf(buf+n, "%s\n", bt_str);
+	flush();
 
-	setColor(B_OFF);
-	setColor(ON);
+	setColor(G_OFF);
+	setColor(B);
 
 	// Connect
 	ret[4] = WyzBee_SPPConnet(bt_str);
+	flush();
+	
+	setColor(B_OFF);
+	setColor(ON);
+
 	ret[5] = WaitForSPPConnComplete();
+	flush();
 
-	setColor(OFF);
+	setColor(OFF);	
+}
 
+void printDevices()
+{
+	// Print Available Devices
+	for(int i = 0; i < NUM_DEVICES; i++) {
+		n += sprintf(buf+n, "%d) %s\n", i, res->RemoteDeviceName);
+	}
+}
+
+void printRetVals()
+{
 	// Print return values
-	n += sprintf(buf+n, "\nret:");
+	n += sprintf(buf+n, "ret:");
 	for(int i = 0; i < 5; i++)
     n += sprintf(buf+n, " %d", ret[i]);
-	n += sprintf(buf+n, "\n");
-	
+}
+
+void flush()
+{
+	printRetVals();
+	oled.setCursor(0,oled.getCursorY());
 	oled.writeString(buf, n);
+	n = 0;
 }
