dd1f49356593faf8ec3154c6438ae9f2cb4878d2
Joseph McGee <jmmcgee.00@gmail.com>
2015-05-11 00:34:01 -0700

    Attempting to get bluetooth working.

--- App/main.cpp
+++ App/main.cpp
@@ -22,15 +22,17 @@ int main()
 	int ret[4] = {0};
 	ret[0] = WyzBee_SetDiscoverMode(1, 1000);
 	ret[1] = WyzBee_SetConnMode(1);
+	ret[2] = WyzBee_InitSppProfile();
 	
+	RSI_BT_EVENT_INQUIRY_RESPONSE p_scan_res;
+	uint8 size = sizeof(p_scan_res);
+
+	//ret[3] = WyzBee_GetInquiryResults(&p_scan_res, size);
+
 	WyzBee_GetLocalName((uint8_t*)buf);
   n = strlen(buf);
-	n += sprintf(buf+n, " %d \n", n);
-  oled.setCursor(0,0);
-	oled.writeString(buf, n);
 	
-	n = 0;
-	n += sprintf(buf+n, "ret:");
+	n += sprintf(buf+n, "\nret:");
 	for(int i = 0; i < 4; i++)
 		n += sprintf(buf+n, " %d", ret[i]);
 	n += sprintf(buf+n, "\n");
