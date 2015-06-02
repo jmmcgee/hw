b4bd3269865fbdcd44f7fcc2e469c119e10505e7
Joseph McGee <jmmcgee.00@gmail.com>
2015-06-01 23:19:31 -0700

    Get works.  Data put through url, and received in resp

--- App/main.cpp
+++ App/main.cpp
@@ -53,15 +53,12 @@ rsi_scanInfo  				remote_dev[RSI_AP_SCANNED_MAX];
 HttpRequest  					http_req;
 volatile 	uint8 			fflag = 0;
 volatile 	uint32_t 		time = 0;
-
 int8     							scan_dev[11][34];
 int8     							message[50];
 int8     							http_resp[1000];
 int8   								*p_ssid_name;
-int8								  status, ix, nbr_scan_devs;
-
-
-
+int8								  ix, nbr_scan_devs;
+int16                 status;
 err_t 								err;
 
 
@@ -85,15 +82,6 @@ WyzBeeSpi_Config_t  config_stc={
 
 //Insert function to print to OLED display (Lab1).
 int printToOLED(char* stringToPrint, int color, int cursorX, int cursorY){
-//#define	BLACK           0x0000
-//#define	BLUE            0x001F
-//#define	RED             0xF800
-//#define	GREEN           0x07E0
-//#define CYAN            0x07FF
-//#define MAGENTA         0xF81F
-//#define YELLOW          0xFFE0
-//#define WHITE           0xFFFF
-	
 	//set color of text
 	oled.setTextColor(color);
 	
@@ -103,7 +91,7 @@ int printToOLED(char* stringToPrint, int color, int cursorX, int cursorY){
 	oled.setCursor(cursorX,cursorY);
 	//print the string to the screen moving the cursor each time
 	for(int i=0; i < stringLength; i++){
-			oled.setCursor(10*i+cursorX,cursorY); //set the print cursor to the inital location
+			oled.setCursor(cursorX+6*i,cursorY); //set the print cursor to the inital location
 			oled.write(stringToPrint[i]); //write a char to screen
 	}
 	
@@ -115,9 +103,11 @@ int printToOLED(char* stringToPrint, int color, int cursorX, int cursorY){
 
 
 
+#define ROW(y) (8*(y))
 
 int main(void)
 {
+  int i =0;
 	err_t err;
 	sys_ticks_init();																				/*Initialize Systick timer*/
 	/*
@@ -134,50 +124,54 @@ int main(void)
 	*/
 	// Initialize the OLED display according to Lab1
 	err_t SPIErr = WyzBeeSpi_Init(&config_stc); //initialize spi
-	status = WyzBeeWiFi_Init(); 
 	//OLED
 	oled.begin(); //OLED screen Initialization
 	oled.fillScreen(CYAN); //@fills the OLED screen with black pixels
 	oled.setTextSize(1); //OLED set text size
-	printToOLED("It's alive", RED, 0,0);
+	printToOLED("It's alive", RED, 0, ROW(i++));
 	
-	char urlbuf[336] = "http://calbeedemo.appspot.com/greetings?msg=hello";
+	char urlbuf[336] = "http://calbeedemo.appspot.com/greetings?msg=IRIS";
 	char statusbuf[336];
+	int n;
 	
-			memset(scan_dev,'\0',sizeof(scan_dev));
-		//status = WyzBeeWiFi_Init();  					                 //@ initializes the RS9113 wifi module
-	  if(status == 0) 
-		{
-					printToOLED("Init works", BLACK, 0,10);
-					
-					nbr_scan_devs = WyzBeeWiFi_ScanNetworks(remote_dev); //@ this functions scans the AP's in vicinity and returns the number of networks scanned.
-					for (ix = 0; ix < nbr_scan_devs; ix++)
-					{							                       //@ returns pointer to a string of each SSID scanned in the network.
-						memcpy (scan_dev[ix], remote_dev[ix].ssid, strlen ((const int8 *)remote_dev[ix].ssid)); 	   //@ copying the all SSIDs into a local buffer
-					}
-					
-					
-					status = WyzBeeWiFi_ConnectAccessPoint((int8 *)"Wireless Bra", (int8 *)"andpanties2"); //join with access point
-				
-					if (status == 0)
-					{
-						// connection access works
-						printToOLED("ConnAcc works", BLACK, 0, 20);
-						
-						status = WyzBeeWiFi_HttpGet((int8 *)urlbuf, &http_req, NULL, NULL);
-						sprintf(statusbuf, "%d", status);
-						printToOLED("Test", BLACK, 0, 30);
+  memset(scan_dev,'\0',sizeof(scan_dev));
+	
+  status = WyzBeeWiFi_Init(); 
+	if(status != 0)
+		return 1;
+  printToOLED("Init works", BLACK, 0,ROW(i++));
 
-						if (status == 0)
-						{
-							printToOLED("HttpGet works!", BLACK, 0, 50);
-						}
-										
-					}
-				}
-		return 0;
-			}
+  //@ this functions scans the AP's in vicinity and returns the number of networks scanned.
+  //@ returns pointer to a string of each SSID scanned in the network.
+  nbr_scan_devs = WyzBeeWiFi_ScanNetworks(remote_dev);
+  for (ix = 0; ix < nbr_scan_devs; ix++) {
+    //@ copying the all SSIDs into a local buffer
+    memcpy (scan_dev[ix], remote_dev[ix].ssid, strlen ((const int8 *)remote_dev[ix].ssid)); 
+  }
+  
+  //join with access point
+  status = WyzBeeWiFi_ConnectAccessPoint((int8 *)"Wireless Bra", (int8 *)"andpanties2"); 
 	
+  if(status != 0)
+    return 1;
+  
+  // connection access works
+  printToOLED("ConnAcc works", BLACK, 0, ROW(i++));
+	
+  uint16 len = 256;
+  int8 data[256];
+  status = WyzBeeWiFi_HttpGet((int8 *)urlbuf, &http_req, data, len);
+  oled.setCursor(0, ROW(i++));
+  
+  sprintf(statusbuf, "Status is: %d\n", status);
+  printToOLED(statusbuf, BLACK, 0, ROW(i++));
+
+  if (status == 0)
+    printToOLED("HttpGet works!", BLACK, 0, ROW(i++));
+  printToOLED(data, BLACK, 0, ROW(i++));
+
+  return 0;
+}	
 /*
  *********************************************************************************************************
  *                                           END
